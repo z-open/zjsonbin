453046e7d21973ee62ef849be45a1e1acac36470
Jacob Friesen <jacob@jacobfriesen.com>
2019-03-19 11:00:01 -0400

    (feat) Fix undefined check issue and String() and Number() issue

--- test/specs/zjson-bin.spec.js
+++ test/specs/zjson-bin.spec.js
@@ -1,3 +1,4 @@
+'use strict';
 const zjsonbin = require('../../lib/zjson-bin.js');
 
 describe('zjsonbin', () => {
@@ -13,23 +14,64 @@ describe('zjsonbin', () => {
     expect(obj).toEqual(r);
   });
 
-  fit('should handle null and undefined array values', () => {
+  it('should handle null and undefined array values', () => {
+    const nowString = '2019-03-19T23:59:59.999Z';
+    const now = new Date('2019-03-19T23:59:59.999Z');
+
     const obj = {
       lastname: 'you',
       firstname: 'John',
       items: [
         'item 1',
+        '',
         null,
         undefined,
+        {},
         42,
-        'item 2',
+        new Number(42),
+        new String('item 2'),
+        now,
       ],
+      nested: {
+        items: [
+          'item 1',
+          null,
+          undefined,
+          42,
+          'item 2',
+        ],
+      },
       property3: null,
     };
 
     const ser = zjsonbin.serialize(obj);
     const r = zjsonbin.deserialize(ser);
-    expect(obj).toEqual(r);
+    expect(r).toEqual({
+      lastname: 'you',
+      firstname: 'John',
+      items: [
+        'item 1',
+        '',
+        null,
+        null,// undefineds transformed to nulls
+        {},
+        42,
+        42,
+        new String('item 2'),
+        nowString,// Dates are auto transformed to a simple date string
+      ],
+      nested: {
+        items: [
+          'item 1',
+          null,
+          null,// undefineds transformed to nulls
+          42,
+          'item 2',
+        ],
+      },
+      // Eliminated, it has no value
+      // property3: null,
+    });
   });
 
   it('should return the same object after deserialization and not crash', () => {
